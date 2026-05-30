a400753f78387e92845a654c0bb70d26349d5a8a
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-05-30 19:18:42 +0530

    Bug removed

--- server.js
+++ server.js
@@ -2,22 +2,21 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 const http = require("http");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 require("dotenv").config();
-const battleHandler = require("./socket/battleHandler")
-
+const battleHandler = require("./socket/battleHandler");
 
 connectToDB();
 
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(app)
-const io = new Server(server,{
-    cors:{origin:"*"}
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: ["http://localhost:5173", process.env.CLIENT_URL] },
 });
 
 //Socket logic attached
 battleHandler(io);
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
