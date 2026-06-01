d9f4ab02e696d7069f58b262d3cfe38ac7bd93d2
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-06-01 14:44:56 +0530

    Server side socketUIo upadted

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173", process.env.CLIENT_URL] },
+  cors: { origin: ["http://localhost:5173", process.env.CLIENT_URL],
+    credentials:true,
+    methods:["GET","POST"],
+   },
 });
 
 //Socket logic attached
