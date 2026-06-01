6638b7cb774ec528e78d20ffd90389c7704e9d66
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-06-01 14:25:21 +0530

    Upadted coockie settings

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -36,7 +36,12 @@ async function registerUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
   res.status(201).json({
     message: "User registered successfully",
@@ -90,7 +95,12 @@ async function logoutUesrController(req, res) {
   if (token) {
     await tokenBlacklistModel.create({ token });
   }
-  res.clearCookie("token");
+  
+  res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
   res.status(200).json({
     message: "User logged out successfully",
