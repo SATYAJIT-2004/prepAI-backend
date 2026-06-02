cf71278dcbdd72f4dc24a8c4180d05fec51fc4be
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-06-02 15:13:17 +0530

    Puppeteer error resolved

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -191,7 +191,10 @@ ${jobDescription}
 }
 
 async function generatePdfFromHtml(htmlContent) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless:true,
+    args:["--no-sandbox","--disabled-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
